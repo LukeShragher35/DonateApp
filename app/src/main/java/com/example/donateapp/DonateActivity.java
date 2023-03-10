f052b1437ecd7fe9e3edd90f4677b0f28c67bf39
LukeShragher35 <114105266+LukeShragher35@users.noreply.github.com>
2023-03-10 01:10:46 -0500

    Login

--- app/src/main/java/com/example/donateapp/DonateActivity.java
+++ app/src/main/java/com/example/donateapp/DonateActivity.java
@@ -3,12 +3,37 @@ package com.example.donateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class DonateActivity extends AppCompatActivity {
+
+    private TextView WELCOME;
+    private TextView PLEAD;
+    private RadioGroup BOX;
+    private RadioButton CHECK;
+    private RadioButton DIRECT;
+    private EditText NUMBERPICK;
+    private Button UPLOAD;
+    private ImageView ART;
+    private ProgressBar UNO;
+    private ProgressBar DOS;
+    private TextView AMOUNT;
+    private EditText DONATEAPP;
+    private Button DONATE;
+    private TextView TOTALFAR;
+    private EditText TOTAL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
-    }
+
+        WELCOME = (TextView)findViewById(R.id.welcome);
+        PLEAD = (RadioGroup)findViewById(R.id.box);}
 }
--- app/src/main/java/com/example/donateapp/LoginActivity.java
+++ app/src/main/java/com/example/donateapp/LoginActivity.java
@@ -2,13 +2,63 @@ package com.example.donateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText USERNAME;
+    private EditText PASSWORD;
+    private Button LOGIN;
+    private Button CANCEL;
+    private ImageView GANNONLOGO;
+    private TextView TITLE;
+    private TextView ATTEMPTS;
+    private int COUNT = 5;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        USERNAME = (EditText)findViewById(R.id.username);
+        PASSWORD = (EditText)findViewById(R.id.password);
+        LOGIN = (Button) findViewById(R.id.login);
+        CANCEL = (Button) findViewById(R.id.cancel);
+        GANNONLOGO = (ImageView)findViewById(R.id.imageView);
+        TITLE = (TextView)findViewById(R.id.textView);
+        ATTEMPTS = (TextView)findViewById(R.id.numattempts);
+
+        ATTEMPTS.setText("Number of Attempts Remaining: 5");
+        LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(USERNAME.getText().toString(), PASSWORD.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPass){
+        if((userName == "username") && (userPass == "password")){
+            Intent intent = new Intent(LoginActivity.this, DonateActivity.class);
+            startActivity(intent);
+        } else{
+            COUNT--;
+
+            ATTEMPTS.setText("Number of Attempts Left: " + String.valueOf(COUNT));
+
+            if(COUNT == 0){
+               LOGIN.setEnabled(false);
+           }
+        }
     }
 }
